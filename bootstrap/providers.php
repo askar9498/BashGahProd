07d9fbce962b6a42daa737bf7335236a1d69c2ea
askar <askarmoslehi00098@gmail.com>
2025-07-01 10:32:41 +0330

    init

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Services\Sms\SmsServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+];
