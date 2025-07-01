6c33637954651692ccbd14a599cd62fc6741cd01
askar <askarmoslehi00098@gmail.com>
2025-07-01 11:01:32 +0330

    add

---
+++ _ide_helper_models.php
@@ -0,0 +1,34 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters query()
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|PsychologicalCenters whereUpdatedAt($value)
+ */
+	class PsychologicalCenters extends \Eloquent {}
+}
+
